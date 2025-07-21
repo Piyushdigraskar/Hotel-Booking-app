a5091e50ab4a4652c22b6b0e358b33fc97da1a21
Piyushdigraskar <piyushdigraskar3@gmail.com>
2025-07-21 11:11:05 +0530

    logo change personalized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,17 @@ import { useLocation } from "react-router-dom";
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
 
-  return <div>{!isOwnerPath && <Navbar />}</div>;
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+
+      <div
+        style={{ height: "2000px", background: "#f5f5f5", paddingTop: "120px" }}
+      >
+        <h1 style={{ textAlign: "center" }}>Scroll down to test navbar</h1>
+      </div>
+    </div>
+  );
 };
 
 export default App;
